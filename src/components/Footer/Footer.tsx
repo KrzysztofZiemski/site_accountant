edfb7b70b306446272c3d3378513fdd30f6eaefa
Krzysztof Ziemski <k.b.ziemski@gmail.com>
2021-02-28 16:01:56 +0100

    remoce navigation from footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -24,29 +24,8 @@ export const Footer = () => {
   const { file } = useStaticQuery(query)
 
   return (
-    <footer className="bg-secondary flex flex-col sm:flex-row sm:flex-wrap text-white md:flex-row md:justify-between  md:flex-wrap lg:flex-nowrap">
-      <ul className="my-3 mx-auto sm:w-2/5 text-center text-md md:text-left md:w-auto md:ml-9 lg:order-2">
-        <li className="m-2">
-          <Link to={routes.main}>strona główna</Link>
-        </li>
-        <li className="m-2">
-          <Link to={routes.about}>o nas</Link>
-        </li>
-
-        <li className="m-2">
-          <Link to={routes.offer}>oferta</Link>
-        </li>
-        <li className="m-2">
-          <Link to={routes.articles}> aktualności</Link>
-        </li>
-        <li className="m-2">
-          <Link to={routes.charity}>działalność charytatywna</Link>
-        </li>
-        <li className="m-2">
-          <Link to={routes.contact}>kontakt</Link>
-        </li>
-      </ul>
-      <address className="p-2 pt-2 sm:w-2/5 md:flex-grow md:text-base md:flex md:justify-around items-center md:order-2 lg:order-3">
+    <footer className="bg-secondary flex flex-col md:flex-row text-white md:justify-between">
+      <address className="p-2 pt-2 sm:w-2/5 md:flex-grow md:text-base md:flex md:justify-around items-center md:order-2 lg:order-2">
         <div className="mb-4">
           <p className="leading-9">GODZINY OTWARCIA</p>
           <p className="leading-9">PN - PT: 8 - 19</p>
@@ -71,7 +50,7 @@ export const Footer = () => {
         </div>
       </address>
 
-      <div className="w-full md:order-3 md:flex md:justify-center lg:order-1 lg:w-2/5">
+      <div className="w-full md:order-1 md:w-2/5">
         <Image
           className="footer-image h-full w-full"
           fluid={file.childImageSharp.fluid}
