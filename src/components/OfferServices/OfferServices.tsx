234f2cc73a1eda157fdc175bf7d1eb9a1c76bce3
krzysztof <k.b.ziemski@gmail.com>
2021-04-06 21:21:44 +0200

    cd ...

--- src/components/OfferServices/OfferServices.tsx
+++ src/components/OfferServices/OfferServices.tsx
@@ -11,36 +11,39 @@ import { routes } from "../../routes"
 export const OfferServices = () => {
   return (
     <div className="flex flex-col items-center p-2 flex-wrap text-primary sm:flex-row justify-center md:justify-around lg:justify-center">
-      <div className="flex justify-start items-center text-bold p-4 mb-2 sm:flex-col lg:m-10">
-        <Link to={routes.offer}>
+      <Link to={routes.offer}>
+        <div className="flex justify-start items-center hover:cursor-pointer	 text-bold p-4 mb-2 sm:flex-col lg:m-10">
           <IconAccountant
             alt="księgowość"
             className="w-auto h-20 md:mb-5"
           ></IconAccountant>
-        </Link>
-        <p className="ml-4 text-2xl">
-          <strong>Księgowość</strong>
-        </p>
-      </div>
-      <div className="flex justify-start items-center text-bold p-4 mb-2 sm:flex-col lg:m-10">
-        <Link to={routes.offer}>
+
+          <p className="ml-4 text-2xl">
+            <strong>Księgowość</strong>
+          </p>
+        </div>
+      </Link>
+      <Link to={routes.offer}>
+        <div className="flex justify-start items-center text-bold p-4 mb-2 sm:flex-col lg:m-10">
           <IconHr alt="kadry i płace" className="w-auto h-20 md:mb-5"></IconHr>
-        </Link>
-        <p className="ml-4 text-2xl">
-          <strong>Kadry i płace</strong>
-        </p>
-      </div>
-      <div className="flex justify-start items-center text-bold p-4 mb-2 sm:flex-col lg:m-10">
-        <Link to={routes.offer}>
+
+          <p className="ml-4 text-2xl">
+            <strong>Kadry i płace</strong>
+          </p>
+        </div>
+      </Link>
+      <Link to={routes.offer}>
+        <div className="flex justify-start items-center hover:cursor-pointer	 text-bold p-4 mb-2 sm:flex-col lg:m-10">
           <IconConsultant
             alt="konsultacje"
             className="w-auto h-20 md:mb-5"
           ></IconConsultant>
-        </Link>
-        <p className="ml-4 text-2xl">
-          <strong>Konsultacje</strong>
-        </p>
-      </div>
+
+          <p className="ml-4 text-2xl">
+            <strong>Konsultacje</strong>
+          </p>
+        </div>
+      </Link>
     </div>
   )
 }
